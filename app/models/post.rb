1ffb8c8bf5bf775f53e0102d34c09d8e257ee9de
TatsuhiroAikawa <tacho119@gmail.com>
2017-10-19 07:16:16 +0000

    show画面修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
     validates :content, {presence: true}
+    #実験中 validates :user_id, {presence: true}
 end
--- db/migrate/20171018051924_create_posts.rb
+++ db/migrate/20171018051924_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.text :content
-
+      
       t.timestamps
     end
   end
