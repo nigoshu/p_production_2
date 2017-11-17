787919dfe015bcd9453595f5cd4040add3be7c50
TatsuhiroAikawa <tacho119@gmail.com>
2017-11-17 10:02:21 +0000

    ログイン認証

--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -1,2 +1,13 @@
 class TwitterUser < ApplicationRecord
+    #引数に関連するユーザーが存在すればそれを返し、存在しまければ新規に作成する
+  def self.find_or_create_from_auth_hash(auth_hash)
+#OmniAuthで取得した各データを代入していく
+    provider = auth_hash[:provider]
+    uid = auth_hash[:uid]
+    nickname = auth_hash[:info][:nickname]
+
+    TwitterUser.find_or_create_by(provider: provider, uid: uid) do |user|
+      user.nickname = nickname
+    end
+  end
 end
